02e59f87579f4699c1ef0990659e4af6c541b9c4
Jake Martin <jcmartin987@gmail.com>
2021-06-18 19:57:23 -0600

    Update emulator with wood frame similar to actual hardware piece

--- Python Emulator/HardwareEmulator.py
+++ Python Emulator/HardwareEmulator.py
@@ -1,17 +1,23 @@
+# Import the graphical library pygame
 import pygame
 
+# Python-only color adjustment function to make "off" circles still appear on the black background
 def color_adjust(color):
     return tuple([round((235/255)*ch+20) for ch in color])
 
+# A class that implements an emulation of the hardware 25x20 lightboard
 class Lightboard:
+    # Lightboard constructor, with optional fps parameter
     def __init__(self, fps=30):
         # Initialize the pygame engine
         pygame.init()
         # Set the font with (size, bold, italics)
         self.font = pygame.font.SysFont('Calibri', 12, True, False)
-        # Set the height and width of the screen to accomodate 25x20 lightboard circles
-        self.size = ((30+5)*25+5, (30+5)*20+5)
+        # Set the height and width of the screen to accomodate 25x20=500 lightboard pixels of
+        # diameter 30px with 5px margin on all sides
+        self.size = ((30+5)*25+5+80, (30+5)*20+5+70)
         self.screen = pygame.display.set_mode(self.size)
+        self.background = pygame.image.load("wood.jpg")
         pygame.display.set_caption("Board Tester v2")
         # Set the FPS rate
         self.FPS = fps
@@ -24,15 +30,16 @@ class Lightboard:
     def display(self, leds:list):
         # Clear the screen and set the screen background
         self.screen.fill((0,0,0))
+        self.screen.blit(self.background, (0,0))
         # Iterates over each of the board pixels to display it
         i=0
         for y in range(20-1,0-1,-1):
             x_path = (0,25,1) if y%2==1 else (25-1,0-1,-1)
             for x in range(*x_path):
-                pygame.draw.ellipse(self.screen, color_adjust(leds[i]), [(30+5)*x+5,(30+5)*y+5,30,30])
+                pygame.draw.ellipse(self.screen, color_adjust(leds[i]), [(30+5)*x+5+40,(30+5)*y+5+35,30,30])
                 if self.disp_nums:
                     text = self.font.render(str(i),True,(255,255,255))
-                    self.screen.blit(text, [(30+5)*x+5,(30+5)*y+5])
+                    self.screen.blit(text, [(30+5)*x+5+40,(30+5)*y+5+35])
                 i+=1
         # Put updated pixels on the screen
         pygame.display.flip()
