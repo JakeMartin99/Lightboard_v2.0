7eb5a3ae65ed96f917358a55c750bd8da1c31e31
Jake Martin <jcmartin987@gmail.com>
2021-06-18 19:05:08 -0600

    Updating python comments

--- Python Emulator/BoardTester_v2.py
+++ Python Emulator/BoardTester_v2.py
@@ -4,19 +4,26 @@ New python code to test board graphics using pygame
 Updated 2021
 """
 
-# Import libraries
+# Import custom libraries
 from HardwareEmulator import Lightboard
 from ModeSet import Modes
 
+# Create object holding all of the lightboard modes
 modes = Modes()
 
-board = Lightboard(fps=30)
-# Initialize the led strip color list
+# Create a object for the hardware emulation
+board = Lightboard()
+# Initialize the 500px (R,G,B) color array
 leds = [(0,0,0) for c in range(500)]
 
+# Loop for as long as the board is on, and each generation...
 while not board.done:
+    # Handle input events
     board.handle_events()
-    leds = modes.text(leds,"BTC 6.9", (255,120,0))
+    # Assign the pixel array based on a mode
+    leds = modes.buff2(leds)
+    # Display the pixel array to the emulated board
     board.display(leds)
 
+# Once done, "turn off" the board to properly terminate the emulation
 board.turn_off()
